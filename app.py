76babb5e68fa3f6d0291d98bfa4a5082115750f6
herossilverio <heros.figueira@gmail.com>
2025-05-26 21:53:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask_cors import CORS
 from utils.calculos import calcular_resina
 
 app = Flask(__name__)
-CORS(app)  # Permite conexões externas (como do frontend no Vercel)
+CORS(app)
 
 @app.route('/calcular', methods=['POST'])
 def calcular():
-    dados = request.get_json()  # Pega o corpo da requisição JSON
+    dados = request.get_json()
     resultado = calcular_resina(dados)
     return jsonify(resultado)
 
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Render define essa variável automaticamente
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
